cd39d7f03fd3f33ba68eaf6acb817f6aaf2ef3fc
Aleksandr Shpuntenko <shpuntenko.a@gmail.com>
2021-07-24 10:52:10 +1000

    Optimized memory for Bellmand-Ford alg.

--- algs/graphs/bellman_ford_alg.py
+++ algs/graphs/bellman_ford_alg.py
@@ -7,21 +7,22 @@ def shortest_path(G : nx.DiGraph, s, t):
     nodesIndices = {nodesLst[i]:i for i in range(n)} 
     
     inf = float("inf")
-    opt = numpy.zeros((n, n))
-    for nodeIndex in range(n): opt[nodeIndex, 0] = inf
-    opt[nodesIndices[t], 0] = 0 
+    opt = [inf] * n
+    opt[nodesIndices[t]] = 0.0
 
     predecessor = [None] * (n+1)
-    for edgesCount in range(1, n):
+    foundNewMin = False 
+    for i in range(1, n):
         for nodeIndex in range(n):
             edges = G.edges(nodesLst[nodeIndex], data = "weight")
-            opt[nodeIndex, edgesCount] = opt[nodeIndex, edgesCount-1]
             for _, w, cost in edges:                 
-                wOpt = cost + opt[nodesIndices[w], edgesCount-1]
-                if wOpt < opt[nodeIndex,edgesCount]: 
-                    opt[nodeIndex,edgesCount] = wOpt
+                wOpt = cost + opt[nodesIndices[w]]
+                foundNewMin = foundNewMin or wOpt < opt[nodeIndex]
+                if foundNewMin: 
+                    opt[nodeIndex] = wOpt
                     predecessor[nodeIndex] = w
+        if not foundNewMin: break 
 
     path = [s]
     while path[-1] != t: path.append(predecessor[nodesIndices[path[-1]]])        
-    return path, opt[nodesIndices[s], n-1]
+    return path, opt[nodesIndices[s]]
