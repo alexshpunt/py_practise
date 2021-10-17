982780c4c76f6f6dffc30bacb4f7ac553a08b5f4
Aleksandr Shpuntenko <shpuntenko.a@gmail.com>
2021-10-17 19:19:11 +1000

    Added scaling parameter for Ford Fulkerson alg as an optimization.

--- algs/flow/ford_fulkerson_alg.py
+++ algs/flow/ford_fulkerson_alg.py
@@ -16,20 +16,20 @@ def construct_path(pred : dict, s, t):
 def ford_fulkerson(G : nx.DiGraph, s, t):
     def f(u, v):  return Gf.get_edge_data(u,v)["flow"]
     def c(u, v): return Gf.get_edge_data(u,v)["capacity"] 
-    def cf(u, v): return c(u,v) - f(u,v) 
-    def bottleneck(p): return min([cf(*e) for e in p])
+    def residual_c(u, v): return c(u,v) - f(u,v) 
+    def bottleneck(p): return min([residual_c(*e) for e in p])
     
     def push_flow(u,v, df):
         Gf.get_edge_data(u,v)["flow"] += df 
         if (v,u) in Gf.edges(): Gf.get_edge_data(v,u)["flow"] -= df 
 
-    def find_aug_path(s, t): 
+    def find_aug_path(s, t, scale = 1): 
         pred = {} 
         stack = [s] 
         while stack: 
             cur = stack.pop() 
             for u,v in Gf.edges(cur): 
-                if cf(u,v) == 0 or v in pred.keys(): continue 
+                if residual_c(u,v) < scale or v in pred.keys(): continue 
                 stack.append(v)
                 pred[v] = u 
         if not t in pred.keys(): return [] 
@@ -41,12 +41,18 @@ def ford_fulkerson(G : nx.DiGraph, s, t):
         if u != s and v != t: Gf.add_edge(v, u, capacity = 0, flow = 0) 
         
     flow = 0 
-    while True: 
-        path = find_aug_path(s, t)
-        if not path: break 
-        df = bottleneck(path)
-        for u,v in path: push_flow(u, v, df)
-        flow += df         
+
+    max_s_capacity = max([Gf.get_edge_data(*e)['capacity'] for e in Gf.edges(s)])
+    scale = pow(2, math.floor(math.log(max_s_capacity)))
+    while scale >= 1: 
+        print(scale)    
+        while True: 
+            path = find_aug_path(s, t, scale)
+            if not path: break
+            b = bottleneck(path)
+            for u,v in path: push_flow(u, v, b)
+            flow += b  
+        scale /= 2  
     print(flow)
 
 ford_fulkerson(FlowNetwork1, 's', 't')
